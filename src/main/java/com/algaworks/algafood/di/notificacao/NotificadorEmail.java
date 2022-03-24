018929871a797957500b89137dd8b3b4deb307ea
Samuel Guimarães <samuca.guimaraes@gmail.com>
2022-03-23 22:06:38 -0300

    Incluído a classe NotificadorProperties para organizar a customização de configurações

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
@@ -7,9 +8,15 @@ import com.algaworks.algafood.di.modelo.Cliente;
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 @Component
 public class NotificadorEmail implements Notificador {
+
+	@Autowired
+	private NotificadorProperties notificadorProperties;
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		
+		System.out.println("Host: " + notificadorProperties.getHostServidor());
+		System.out.println("Porta: " + notificadorProperties.getPortaServidor());
 		
 		System.out.printf("Notificamos %s através do e-mail %s : %s\n", 
 				cliente.getNome(), cliente.getEmail(), mensagem);		
--- src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
+++ src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
@@ -1,12 +1,8 @@
 package com.algaworks.algafood;
 
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+class AlgalogApiApplicationTests {
 
-@SpringBootTest
-class AlgafoodApiApplicationTests {
-
-	@Test
+	
 	void contextLoads() {
 	}
 
