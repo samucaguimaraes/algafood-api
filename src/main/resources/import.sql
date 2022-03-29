2c5381fe8dbe3b2c36ad0b4672ee6ba292deb687
Samuel Guimarães <samuca.guimaraes@gmail.com>
2022-03-29 11:15:10 -0300

    ManyToOne restaurante => cozinha

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
 insert into cozinha (nome) value ('Tailandesa');
 insert into cozinha (nome) value ('Japonesa');
 
-insert into restaurante (nome, taxa_frete) values ("Encheno e Derramando", 4.50);
-insert into restaurante (nome, taxa_frete) values ('Alforria', 6.00);
-insert into restaurante (nome, taxa_frete) values ('Moenda', 7.00);
-insert into restaurante (nome, taxa_frete) values ('Inacio', 8.00);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ("Encheno e Derramando", 4.50,1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Alforria', 6.00, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Moenda', 7.00, 2);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Inacio', 8.00, 2);
